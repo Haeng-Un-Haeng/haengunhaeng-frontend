eea7d247a63fd1dbe390cea2c2026697cfdb0820
jaqwe2301 <jaqwe2301@naver.com>
2026-09-22 18:09:10 +0900

    chore: 코드 검사 스크립트 및 ESLint·Prettier·Husky 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,29 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  {
+    rules: {
+      'no-console': 'warn',
+    },
+  },
+
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'storybook-static/**',
+    'coverage/**',
+    'next-env.d.ts',
   ]),
+
+  eslintConfigPrettier,
 ]);
 
 export default eslintConfig;
